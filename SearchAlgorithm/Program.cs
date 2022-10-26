67af74fa5a911ae237760620c96766a875bf9eff
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-10-26 16:18:11 +0700

    Add method input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -14,6 +14,32 @@ namespace SearchAlgorithm
         int n;
         //get the number of elements to store in the array
         int i;
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter the Number of Elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements. \n");
+            }
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter Array Elements ");
+            Console.WriteLine("----------------------");
+            for ( i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+
+        }
         static void Main(string[] args)
         {
         }
